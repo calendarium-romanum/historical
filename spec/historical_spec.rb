c1df73a2c269c69fd311da0329c9195f0a11b360
Jakub Pavlik <severus@post.cz>
2020-07-23 11:32:24 +0200

    delete boilerplate spec

--- spec/historical_spec.rb
+++ spec/historical_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Calendarium::Romanum::Historical do
   it "has a version number" do
     expect(Calendarium::Romanum::Historical::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
