5808b6786492cb3e617728e5198f0918e41a3010
Jakub Pavlik <severus@post.cz>
2020-07-23 10:09:50 +0200

    boilerplate (bundle gem create calendarium-romanum-historical)

---
+++ lib/calendarium/romanum/historical.rb
@@ -0,0 +1,10 @@
+require "calendarium/romanum/historical/version"
+
+module Calendarium
+  module Romanum
+    module Historical
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/calendarium/romanum/historical/version.rb
@@ -0,0 +1,7 @@
+module Calendarium
+  module Romanum
+    module Historical
+      VERSION = "0.1.0"
+    end
+  end
+end
