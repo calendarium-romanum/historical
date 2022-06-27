684b7bd087d2aa4670f1971c8a12d00475e59543
Jakub Pavlik <severus@post.cz>
2022-06-28 01:18:52 +0200

    specs: ignore celebration titles when comparing latest state
    it would be desirable to have the Latin celebration titles matching
    between calendarium-romanum and calendarium-romanum-historical,
    but for now that's not an issue to be addressed soon.
    
    Note: spec/data_spec.rb pass with fresh development version of
    calendarium-romanum, but not with the last version currently released
    (0.9.0) which lacks a few data fixes.

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe 'data' do
   let(:file) { 'data/general-roman-la.xml' }
   let(:loader) { CR::Historical::SanctoraleLoader.new }
 
+  def without_titles(sanctorale)
+    sanctorale.map_celebrations do |celebration|
+      celebration.change title: '' # empty string, because Celebration#change uses nil as default value meaning "do not change this property, copy it"
+    end
+  end
+
   it 'loads the initial version correctly' do
     historical = loader.load_from_file(file, at: Date.new(1969,1,1))
     cr_bundled = CR::Data::GENERAL_ROMAN_LATIN_1969.load
@@ -14,14 +20,15 @@ RSpec.describe 'data' do
     strip_metadata(historical)
     strip_metadata(cr_bundled)
 
-    # TODO: now literal comparison (including celebration titles) fails on a single apostrophe
     expect(historical)
       .to eq cr_bundled
   end
 
   it 'loads the current version and it is complete' do
-    expect(loader.load_from_file(file, at: Date.today))
-      .to eq CR::Data::GENERAL_ROMAN_LATIN.load
+    # TODO: it would be desirable for this example to pass also with raw data,
+    #   without the need to strip celebration titles
+    expect(without_titles(loader.load_from_file(file, at: Date.today)))
+      .to eq without_titles(CR::Data::GENERAL_ROMAN_LATIN.load)
   end
 
   # potentially dangerous edge case: given document removes one celebration and introduces
